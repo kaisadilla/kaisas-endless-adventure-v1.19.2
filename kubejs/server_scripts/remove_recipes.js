9ddc06c0145ad140e0bf1f34599424eab5bdd1b8
kaisadilla <58343029+kaisadilla@users.noreply.github.com>
2024-07-25 01:25:40 +0200

    Many things

--- kubejs/server_scripts/remove_recipes.js
+++ kubejs/server_scripts/remove_recipes.js
@@ -3,5 +3,6 @@
 console.info("Loading server_scripts/remove_recipes.js");
 
 ServerEvents.recipes(evt => {
+	evt.remove({ mod: 'minecraft' });
 	evt.remove({ output: 'minecraft:stone_pickaxe' });
 });
