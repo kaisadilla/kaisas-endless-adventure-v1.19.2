ca3a6f3b84a86908807e0cb38ecc8e7d223e9f6e
kaisadilla <58343029+kaisadilla@users.noreply.github.com>
2024-08-28 17:42:08 +0200

    Many things

--- kubejs/server_scripts/remove_recipes.js
+++ kubejs/server_scripts/remove_recipes.js
@@ -3,16 +3,35 @@
 console.info("Loading server_scripts/remove_recipes.js");
 
 ServerEvents.recipes(evt => {
+	// remove all recipes from mod
 	evt.remove({ mod: 'minecraft' });
 	evt.remove({ mod: 'tconstruct' });
 	evt.remove({ mod: 'quark' });
 	evt.remove({ mod: 'supplementaries' });
+	evt.remove({ mod: 'storagedrawers' });
+
+	// remove specific recipes
+	evt.remove({ mod: 'gravestone', output: [
+		'gravestone:gravestone',
+	]});
+
+	evt.remove({ mod: 'iceandfire', output: [
+		'minecraft:gravel',
+	]});
+
+	evt.remove({ mod: 'infernalexp', output: [
+		'minecraft:torch',
+	]});
+
+	evt.remove({ mod: 'thermal', output: [
+		'minecraft:prismarine_shard',
+	]});
+
+	evt.remove({ mod: 'unusualend', output: [
+		'minecraft:glass',
+	]});
+
 	// remove all melting recipes from tconstruct.
 	// evt.remove({ mod: 'tconstruct', type: 'tconstruct:melting'});
 	// evt.remove({ mod: 'tconstruct', type: 'tconstruct:ore_melting'});
-
-	// this will be removed entirely later
-	evt.remove({ mod: 'iceandfire', item: 'minecraft:gravel' });
-	evt.remove({ mod: 'unusualend', item: 'minecraft:glass' });
-	evt.remove({ mod: 'infernalexp', item: 'minecraft:torch' });
 });
